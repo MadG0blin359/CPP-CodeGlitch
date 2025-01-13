51b7fff2138ba0fae7cb85608cda133eb6ca8286
MadG0blin359 <shawaizshahid312+github@gmail.com>
2025-01-13 17:47:59 +0500

    Removed Practice

--- Practice/stringtest.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-
-int main() {
-    std::string myString = "boss\n\0";  // Length function counts until it encounters \0, however \0 is automatically added to a string without adding it explicitly, its not included in the count.
-    std::cout << "Length of the string: " << myString.length() << std::endl;
-
-    return 0;
-}
